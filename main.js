f968e4b6e4fd3833b41a0cdfad8ac9724a69a6ef
Ronak.T <ronakcoder2@gmail.com>
2026-01-04 17:45:50 -0500

    Refactor button event listener for song playback

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
+
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+document.body.appendChild(tag);
+
+
+const players = [];
+
+
+function onYouTubeIframeAPIReady() {
+  const iframes = document.querySelectorAll('iframe');
+
+  iframes.forEach((iframe, index) => {
+    players[index] = new YT.Player(iframe, {
+      events: {}
+    });
+  });
+}
+
+
 const buttons = document.querySelectorAll('.song-button');
 
-buttons.forEach(button => {
+buttons.forEach((button, index) => {
   button.addEventListener('click', () => {
 
 
     button.classList.add('pulse-flash');
-    setTimeout(() => {
-      button.classList.remove('pulse-flash');
-    }, 400); 
+    setTimeout(() => button.classList.remove('pulse-flash'), 400);
 
-
-    const iframe = button.nextElementSibling; 
-    let src = iframe.src;
-
-    if (!src.includes('autoplay=1')) {
-      iframe.src = src + (src.includes('?') ? '&autoplay=1' : '?autoplay=1');
+  
+    if (players[index]) {
+      players[index].playVideo();
     }
   });
 });
