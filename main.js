b0d89226db1f3e499cf84c51816b75c098019071
Ronak.T <ronakcoder2@gmail.com>
2026-01-04 17:49:33 -0500

    Refactor YouTube player initialization and button click handling

--- main.js
+++ main.js
@@ -1,35 +1,30 @@
-
+// Load YouTube IFrame API
 let tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 document.body.appendChild(tag);
 
-
+// Array to store players
 const players = [];
 
-
+// Called when API is ready
 function onYouTubeIframeAPIReady() {
-  const iframes = document.querySelectorAll('iframe');
-
-  iframes.forEach((iframe, index) => {
-    players[index] = new YT.Player(iframe, {
-      events: {}
+    const iframes = document.querySelectorAll('iframe');
+    iframes.forEach((iframe, index) => {
+        players[index] = new YT.Player(iframe);
     });
-  });
 }
 
-
+// Button click: pulse animation + play video
 const buttons = document.querySelectorAll('.song-button');
-
 buttons.forEach((button, index) => {
-  button.addEventListener('click', () => {
+    button.addEventListener('click', () => {
+        // Pulse animation
+        button.classList.add('pulse-flash');
+        setTimeout(() => button.classList.remove('pulse-flash'), 400);
 
-
-    button.classList.add('pulse-flash');
-    setTimeout(() => button.classList.remove('pulse-flash'), 400);
-
-  
-    if (players[index]) {
-      players[index].playVideo();
-    }
-  });
+        // Play video
+        if (players[index]) {
+            players[index].playVideo();
+        }
+    });
 });
